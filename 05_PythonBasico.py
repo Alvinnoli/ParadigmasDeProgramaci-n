79ab0c51249e2262254a66905a14936f593d341b
Alvinnoli <alvin.nolilemus@gmail.com>
2023-09-19 15:28:43 -0600

    05_PythonBasico.py

---
+++ 05_PythonBasico.py
@@ -0,0 +1,38 @@
+#==============================
+#   Alvin NOli Lemus 
+#==============================
+#
+#  Matemática Algoritmica
+#  Paradigmas de programacion 
+#  ESFM IPN Septiembre 2023
+#
+#==============================
+
+#==============================
+#  Condicionales 
+#==============================
+#  Condicionales
+#==============================
+precio=50 
+#---------------------
+# Si esto..... 
+#---------------------
+if precio < 50:
+   print ("El precio es menor que 50")
+#---------------------
+# Si nada de los anterior...
+#---------------------
+else: 
+    print("El precio es 50")
+
+precio = 50 
+cantidad = 5 
+total = precio*cantidad 
+#=============================
+# Condocionales anidados
+#=============================
+if total > 100: 
+    if total > 500: 
+        print ("Total es mayor que 500")
+    else:
+        if total < 500 and total 
