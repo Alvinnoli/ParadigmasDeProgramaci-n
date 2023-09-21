7d92f16eeee7985802eb1598abedbc2c2ab2b9ea
Alvinnoli <alvin.nolilemus@gmail.com>
2023-09-21 15:50:53 -0600

    07_PythonBasico.py

---
+++ 06_PythonBasico_funciones.py
@@ -0,0 +1,40 @@
+#================================
+#  Alvin Noli Lemus 
+#================================
+# Matematica Algoritmica 
+# Paradigmas de programacion 
+# ESFM IPN 
+#================================
+
+
+#================================
+# MI primera funcion
+#================================
+def saludo ():
+    #===============================
+    #  Documentacion rapida de la funcion 
+    #===============================
+    """Esta funcion saluda"""
+    print('¡QUIOBULES!,¿Cómo estás?')
+
+#===========================
+#  llamado de la funcion
+#==========================
+saludo()
+ 
+#========================
+# Se ejecuta pero no se asigna
+#========================
+salida = saludo() 
+
+#=========================
+# ESto no funciona
+#========================
+print (salida)
+
+#========================
+#   Mostrar documentacion 
+#========================
+
+
+
---
+++ 07_PythonBasico.py
@@ -0,0 +1,13 @@
+#===========================================
+#   ALvin Noli Lemus 
+#===========================================
+# Mamtematica Algorítmica 
+# Paradigmas de programacion 
+# ESFM IPN Septiembre 2023
+#===========================================
+#
+#===========================================
+# Importacion de modulos 
+# Contiene objetos y funciones 
+#
+#===========================================
