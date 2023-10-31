9fc1a9f5fbf52292e3707a4da21fb1e1415290bf
Alvinnoli <143754962+Alvinnoli@users.noreply.github.com>
2023-10-30 21:08:53 -0600

    Delete 01_PythonBasico.py

--- 01_PythonBasico.py
+++
@@ -1,131 +0,0 @@
-#==================================================
-# Noli Lemus Alvin 
-#==================================================
-#
-# Paradigmas de Programación 
-# Mamtemática Algoritmica
-# ESFM - IPN sept - 2023 
-#
-#==================================================
-
-''' ESTE ES UN SUPERCOMENTARIO 
-    DE INICIO A NUESTRO RESUMEN 
-'''
-
-#==================================================
-# Operaciones Basicas 
-#==================================================
-print (2+3)
-print (2*3)
-print (2/3)
-print (2**10) 
-print (2**0.5)   #Raiz Cuadrada
-print (10%2)
-print (10%0.1)   #Exclusivo de Python 
-
-#==================================================
-# Para saber el tipo de objetos se usa type
-#==================================================
-t=0
-print (type(t)) # entero 
-t=3.6
-print (type(t)) # real (FLotante)
-t=TRUE
-print (type(t)) # booleano (bool)
-
-#==================================================
-# Mensajes a pantalla 
-#==================================================
-
-print ("Este es un comando de python. ", "Este es otro enunciado.",t)
-print('id: ',1)
-print('First Name: ', 'Steve')
-print('Last Name: ', 'Jobs')
-print("Vamos a sumar esto" + "con esto otro")
-
-#==================================================
-# Continuar una instruccion en varios renglones
-#==================================================
-if 100>99 and  
-   200 <= 300 and 
-    True != False:
-         print ('Hello World')
-
-#===================================================
-# Comandos diferentes en la misma linea
-#===================================================
-print ("Hola "); print("tu!!")   #Se considera mala práctica
-
-#===================================================
-# Usando paréntesis redondos, cuadrados o llaves
-# Se puede escrbiri en varios renglones 
-#===================================================
-list = [1,2,3,4,
-        5,6,7,8,
-        9,10,11,12]
-
-matriz = [ [1,2,3,4],[5,6,7,8],[9,10,11,12] ]
-
-print (list)
-print (matriz)
-
-#===================================================
-# Indentación estricta para procesos dependedientes de : (dos  puntos)
-#===================================================
-if 10>5:
-  print ("dies es mayor que cinco")
-  print ("otra indentación")
-for i in list:
-  print (i)
-  print ("ok")
-if 10>5:
-  print("verdadero")
-  if 10<20: 
-	print("verdadero")
-elif 5>3: #comienza segundo condicional
-  print ("esto no se imprimirá")
-else:
-  print ("aquí nunca llega")
-
-#=====================================================
-# Funciones 
-#=====================================================
-def say_hello(name):
-    print("Hello ",name)
-    print("Welcome to Python Tutoriales")
-
-say_dello("Alvin")
-
-
-
-
-  
-  print (
- 
-D
-if 100>99 and 
-o|°°°°°°°°°°°°°°°°|||||||||||°°°|°°|||
-if 100 > 99 and 
-print (
-A
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
