ccece8b67b38961b3f27c6dae84c93451e7a94f3
Alvinnoli <alvin.nolilemus@gmail.com>
2023-10-31 15:09:01 -0600

    08_ObjetosIntro.py

--- 08_ObjetosIntro.py
+++ 08_ObjetosIntro.py
@@ -84,3 +84,93 @@ class Estudiante:
     def ponerme_nombre(mi, nombre): 
         print('se llamó a ponerme_nombre')
         mi.__nombre= nombre
+    def obtener_nombre(mi):
+        print ('se llamó a obtener_nombre')
+        return mi.__nombre 
+    nombre=property(obtener_nombre,ponerme_nombre)
+
+#========================================
+# Crear objeto estudiante sin nombre 
+#========================================
+estudiante = EStudiante()
+
+#========================================
+# Ponerle nombre usando la propiedad nombre y el metodo ponerme_nombre 
+# (sin tener que llamar explicitamente a la función)
+#========================================
+estudiante.nombre = "Diego" 
+
+#========================================
+# Obtener el nombre con el metodo obtener nombre 
+# __nombre es una variable encapsulada (no visible desde afuera) 
+# (sin tener que llamar explícitamente a la funcion obtener_nombre)
+#=======================================
+print(estudiante. nombre)
+
+# Esto no funciona 
+#print(estudiante.__nombre 
+
+#=======================================
+# Herencia de clases
+#=======================================
+class Cuadrilatero:
+    def __init__ (mi,a,b,c,d):
+        mi.lado1=a 
+        mi.lado2=b
+        mi.lado3=c
+        mi.lado4=d
+
+    def perimetro(mi):
+        p=mi.lado1 + mi.lado2 + mi.lado3 + mi.lado4
+        print ("perimetro=", p)
+        return p 
+#=======================================
+# Su hijo  rectangulo 
+# Rectangulo es hijo de cuadrilatero
+# Rectangulo(cuadrilatero)
+#=======================================
+class Rectangulo (Cuadrilatero): 
+    def __init__ (self, a, b):
+        #====================================
+        # Constructor de su madre
+        #====================================
+        super().__init__(a,b,a,b)
+#====================================
+# Su nieto cuadrado 
+# Hijo de rectangulo 
+#====================================
+class Cuadrado(Rectangulo):
+    def __init__(self, a):
+        super().__init__(a,a)
+
+    def area(self):
+        area = self.lado**2
+        return area 
+
+    #def perimetro(self):
+    #   p=4.0*self.lado1 
+    #   print("perimetro =",p)
+    #   return p
+
+#======================================
+# Crear un cuadrado 
+#======================================
+cuadrado1 = Cuadrado(5)
+
+#=========================================================
+# Llamar al método perímetro de su abuelo cuadrilatero
+#=========================================================
+perimetro1 = cuadrado 1.perimetro() 
+
+#=======================================
+# Llamar a su propio método área
+#=======================================
+area1 = cuadrado1.area()
+
+print("Perímetro =", perimetro1)
+print("Area = ", area1)
+
+#==============================================================
+# Sobre-escribir un método de su madre o abuela o tatarabuela.....
+# Es volver a definir una función ya existente
+#==============================================================
