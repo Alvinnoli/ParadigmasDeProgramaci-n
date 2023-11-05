b7f1cc053982da676f7158e311ed9a1ed953a25e
Alvinnoli <143754962+Alvinnoli@users.noreply.github.com>
2023-11-05 15:32:47 -0600

    03_PythonBasico.py

--- 03_PythonBasico.py
+++ 03_PythonBasico.py
@@ -1,1 +1,79 @@
+#==============================
+#   Alvin NOli Lemus 
+#==============================
+#
+#  Matemática Algoritmica
+#  Paradigmas de programacion 
+#  ESFM IPN Septiembre 2023
+#==============================
+
+#===============================================
+# Listas
+# Las listas pueden ser de objetos diferentes
+#===============================================
+miprimeralsita = []    #Lista vacía
+print(mipriemralista)
+
+#=========================================
+# Llenado de lista
+#=========================================
+miprimeralista = [1,"Javier", 1.34, "Bosco", "Angel","Abigail", True]
+print(miprimeralista)
+
+#=========================================
+# list: hacer una lista
+# range(i,j): secuencia de i hasa j-1
+#=========================================
+nums = list(range(1,61))
+
+for i in nums:
+  print(i)
+
+#=====================================
+# Incluí nuevos elementos en la lista
+#=====================================
+nums.append(61)
+nums.append(62)
+nums.append(61)
+print(nums)
+
+#======================================
+# Quitar elementos de la lista
+#======================================
+nums.remove(61)
+print(nums)
+
+#======================================
+# Quitar elemento con índice i
+#======================================
+i = 61 
+del nums[i]
+print(nums)
+
+#======================================
+# Borrar la lista
+#======================================
+del nums
+
+#======================================
+# Sumar listas
+#======================================
+L1 = [1,2,3]
+L2 = [4,5,6]
+print(L1+l2)
+
+#======================================
+# Llenado a mano
+#======================================
+potencial = [] 
+for i in range(0,10000):
+  potencial.append(float(i))
+print(potnecial[100])
+
+#=======================================
+# Generar una tupla con la lista
+#=======================================
+potencial = tuple(potencial)
+print(potencial[100])
+
 
