5f4e82188ff5032472aa7ac39edc29afca5bace5
Alvinnoli <143754962+Alvinnoli@users.noreply.github.com>
2023-11-07 16:46:53 -0600

    09_Constructores_destructores_Asociación_de_objetos.py

---
+++ 09_Constructores_destructores_Asociación_de_objetos.py
@@ -0,0 +1,19 @@
+#=========================================
+#  Alvin NOli Lemus 
+#=========================================
+# Paradigmas de Programacion 
+# Matematica Algoritmica 
+# ESFM IPN Septiembre 2023
+#=========================================
+
+#=========================================
+# Clase computadora
+#=========================================
+class Computadora:
+  marca: str = None
+  capacidad: int = 0 
+  ram: int = 0 
+
+  #=======================================
+  # Constructor
+  #=========================================
