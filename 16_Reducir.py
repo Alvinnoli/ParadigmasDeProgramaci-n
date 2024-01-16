8a62e55c29d2c4f5cf0965bd5782176a435b2091
Alvinnoli <143754962+Alvinnoli@users.noreply.github.com>
2024-01-16 10:07:58 -0600

    16_Reducir.py

--- 16_Reducir.py
+++ 16_Reducir.py
@@ -23,6 +23,11 @@ bigdata = [2,3,5,7,11,13,19,23,29]
 #================
 multiplicar = lambda x,y: x*y
 
+#================
+# Función x + y
+#================
+suma = lambda x, y: x + y
+
 print(reduce(multiplicar,bigdata))
 print(reduce(suma,bigdata))
 
