a255745fe0300bb8a847a9cfe0cbc299ddb4e22e
Alvinnoli <143754962+Alvinnoli@users.noreply.github.com>
2024-01-17 18:22:39 -0600

    cliente_bancario.py

--- cliente_bancario.py
+++ cliente_bancario.py
@@ -2,58 +2,58 @@
 # Clase ClienteBancario
 #========================
 class ClienteBancario:
-    _nombres:str = None
-    _apellidos:str = None
-    _edad:int = 0 
-    _balanceDeCuenta:float = 0.0
+    __nombres:str = None
+    __apellidos:str = None
+    __edad:int = 0 
+    __balanceDeCuenta:float = 0.0
 
     def __init__(self, nombres:str, apellidos:str, edad:int=0, balanceDeCuenta:float=0.0):
-        self._validarEdad(edad)
-        self._validarCantidad(balanceDeCuenta)
+        self.__validarEdad(edad)
+        self.__validarCantidad(balanceDeCuenta)
         self.nombres = nombres
         self.apellidos = apellidos
-        self._edad = edad 
+        self.__edad = edad 
         self.balanceDeCuenta = balanceDeCuenta
 
   def getNombreCompleto(self)   -> str:
       return self.nombres + " " + self.apellidos
 
-  def _mandarEmail(self, titulo:str, texto:str)  -> None:
+  def __mandarEmail(self, titulo:str, texto:str)  -> None:
       print("Mandar email: " + titulo + " con texto: " + texto)
 
-  def _enviarBalanceAlBanco(self, cantidad:float)  ->None:
+  def __enviarBalanceAlBanco(self, cantidad:float)  ->None:
       print("Enviando cantidad: " + str(cantidad) + "al banco...")
 
   #=================================================
   # Método privado con dos guiones bajos
   # Si la edad es menor que 18 genera un error
   #=================================================
-  def _validarEdad(self, edad:int)   -> None:
+  def __validarEdad(self, edad:int)   -> None:
       if edad < 18:
           raise Exception("Es menor de edad")
 
   def imprimirInfo(self)   -> str:
-      return "Nombre: " + self.getNombreCompleto() + ", Edad: " + str(self._edad) + ", Balance: " + str(self._balanceDeCuenta)
+      return "Nombre: " + self.getNombreCompleto() + ", Edad: " + str(self.__edad) + ", Balance: " + str(self.__balanceDeCuenta)
 
   #======================================================
   # Método privado que checa si el balance es negativo
   # y genera un error 
   #======================================================
-  def _validarCantidad(self, balanceDeCuenta:float)  -> None:
+  def __validarCantidad(self, balanceDeCuenta:float)  -> None:
       if balanceDeCuenta < 0:
           raise Exception("El balance en la cuenta no puede ser negativo")
 
   def guardarDinero (self, cantidad:float)  -> None:
-      self._balanceDeCuenta = self._balanceDeCuenta + cantidad
-      self._mandarEmail("---- guardando depósito -----", " se recibieron " + str(cantidad))
-      self._ enviarBalanceAlBanco(cantidad)
+      self.__balanceDeCuenta = self.__balanceDeCuenta + cantidad
+      self.__mandarEmail("---- guardando depósito -----", " se recibieron " + str(cantidad))
+      self.__ enviarBalanceAlBanco(cantidad)
 
   def retirarDinero(self, cantidad:float)  -> None:
-      cantidadFinal = self._balanceDeCuenta - cantidad
-      self._validarCantidad(cantidadFinal)
-      self._balanceDeCuenta = cantidadFinal
-      self._mandarEmail("----retirando dinero-----", " se retiró " + str(cantidad))
-      self._enviarBalanceAlBanco(cantidad)
+      cantidadFinal = self.__balanceDeCuenta - cantidad
+      self.__validarCantidad(cantidadFinal)
+      self.__balanceDeCuenta = cantidadFinal
+      self.__mandarEmail("----retirando dinero-----", " se retiró " + str(cantidad))
+      self.__enviarBalanceAlBanco(cantidad)
 
 
     
